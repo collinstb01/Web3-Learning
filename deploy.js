a08ddc2456515bc6d428c9835164ac9e7460a822
root <collinstb01@gmail.com>
2022-08-24 04:03:34 -0700

    first commit

--- deploy.js
+++ deploy.js
@@ -7,6 +7,7 @@ async function main() {
   // First, compile this!
   // And make sure to have your ganache network up!
   let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+
   let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
   // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
