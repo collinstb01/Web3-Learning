f97c5d681506fa51f98330f537fffaca88128744
root <collinstb01@gmail.com>
2022-08-25 15:14:34 -0700

    Worked on Deploying Smart Contracts beter way

--- Fundme/contracts/FundMe.sol
+++ Fundme/contracts/FundMe.sol
@@ -51,7 +51,7 @@ contract FundMe {
   /// @notice Funds our contract based on the ETH/USD price
   function fund() public payable {
     require(
-      msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+      PriceConverter.getConversionRate(msg.value, s_priceFeed) >= MINIMUM_USD,
       "You need to spend more ETH!"
     );
     // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
--- Fundme/hardhat.config.js
+++ Fundme/hardhat.config.js
@@ -11,7 +11,7 @@ require("solidity-coverage")
 module.exports = {
   defaultNetwork: "hardhat",
   solidity: {
-    compilers: [{ version: "0.8.9" }, { version: "0.6.0" }],
+    compilers: [{ version: "0.8.9" }, { version: "0.6.6" }],
   },
   networks: {
     rinkbey: {
