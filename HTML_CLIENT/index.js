ec21aeb4da0692c2d8b66cd6fac2b48f2eb656b7
Evbadoloyi Collins Eguasa <collinstb01@gmail.com>
2022-08-27 05:29:19 -0700

    Javascript Promises Check

--- HTML_CLIENT/index.js
+++ HTML_CLIENT/index.js
@@ -1,3 +1,5 @@
+const { ethers } = require("ethers")
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     console.log("I see a metamask")
@@ -9,8 +11,32 @@ async function connect() {
   }
 }
 
+const ethAmouut = 1
 const fund = async (ethAmouut) => {
   console.log(`Funding with ${ethAmouut}`)
+  const provider = await ethers.provider.web3Provider(window.ethereum)
+  const signer = await ethers.getSigners()
+  const contract = await ethers.Contract(provider, abi, signer)
+  // abi
+  // signers address
+  // RFC provider
+  try {
+    const transactionResponse = await contract.fund({
+      value: ethAmouut
+    })
+
+    console.log("Done");
+  } catch (error) {
+    console.log(error)
+  }
+
+  //contract address
 }
 
-const withdraw = async () => {}
+ function listener() {
+    return new Promise = ((resolve, reject) => {
+  provider.once(transactionResponse.hash,() => {
+
+    })
+  })
+ }
